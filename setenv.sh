36713d406acee01dd7556087b30f235c46f23b5f
chgv0926 <chgv0926@gmail.com>
2024-01-12 00:59:44 +0800

    20240112

--- setenv.sh
+++ setenv.sh
@@ -1,1 +1,5 @@
-export JAVA_OPTS="-Ddbpassword=${EMP_DB_PWD} -Ddbusername=${EMP_DB_USR}" -Dhostname=${HOSTNAME}"
+#!/bin/bash
+
+export HOSTNAME=$(hostname)
+
+export JAVA_OPTS="$JAVA_OPTS -Ddbpassword=${EMP_DB_PWD} -Ddbusername=${EMP_DB_USR} -Dhostname=$HOSTNAME"
